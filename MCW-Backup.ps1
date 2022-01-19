e14998943036cc9e5b9971f2b595a078db377338
Kevin Luna <kevin.luna200344@gmail.com>
2022-01-19 16:50:38 -0600

    IDR what I just did,lol

--- MCW-Backup.ps1
+++ MCW-Backup.ps1
@@ -1,17 +1,20 @@
 #Options
 [CmdletBinding()]
 param (
+    
+    #Mandatory parameters
+    [Parameter(Mandatory)]
+    [String]$MinecraftVersion,
+    
     #Optional parameters
     [Parameter()]
     #The default output folder is this
-    [String]$Output = "C:\Users\$env:USERNAME\Minecraft Backups\",
-
-    #Mandatory parameters
-    [Parameter(Mandatory)]
-    [String]$MinecraftVersion
+    [String]$Output = "C:\Users\$env:USERNAME\Minecraft Backups\"
+    #Default file type for output backup is 7z
+    # [String]$FileType = "7z"
 )
 
-
+#Select Minecraft Version
 if($MinecraftVersion -eq "Bedrock"){
     $minecraft_worlds_folder = "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds"
 }elseif($MinecraftVersion -eq "Java"){
@@ -21,19 +24,28 @@ if($MinecraftVersion -eq "Bedrock"){
     exit
 }
 
-#Check if 7zip its installed
-if(Test-Path "C:\Program Files\7-Zip\7z.exe"){
-    Set-Alias -Name Compress -Value "C:\Program Files\7-Zip\7z.exe"
-}else {
-    Write-Host -ForegroundColor Yellow "7zip it's not installed, using Compress-Archive instead..."
-    Set-Alias -Name Compress -Value Compress-Archive
-}
+
+#The option for change the output file format its pendient
+# #Select file output type if it's not default
+# if(($FileType -ne "7z") -or ($FileType -ne "zip") -or ($FileType -ne "gzip") -or ($FileType -ne "bzip2") -or ($FileType -ne "tar")){
+
+# }
+
 
 $date =  Get-Date -Format "dd-MM-yyyy"
-$output_filename = "$Output\Pichichi-$date.7z"
-
+$output_filename = "$Output\Pichichi-$date"
 Write-Host -ForegroundColor Green "Compressing the World..."
-Compress a -t7z $output_filename "$minecraft_worlds_folder\HqG5YEinEgA=" -mx=9 -mmt=on > $null
+
+#Check if 7zip its installed
+if(Test-Path "C:\Program Files\7-Zip\7z.exe"){
+    #you can specify another path for 7zip installation
+    Set-Alias -Name Compress -Value "C:\Program Files\7-Zip\7z.exe"
+    Compress a -t7z "$output_filename.7z" "$minecraft_worlds_folder\HqG5YEinEgA=" -mx=9 -mmt=on > $null
+}else {
+    Write-Host -ForegroundColor Yellow "7zip it's not installed, using Compress-Archive instead..."
+    Compress-Archive -Path "$minecraft_worlds_folder\HqG5YEinEgA=" -DestinationPath "$output_filename.zip"
+}
+
 
 if($LASTEXITCODE -eq 0){
     Write-Host -ForegroundColor Green "Everything is ok"
