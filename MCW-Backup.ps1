7b93b1804b4c2025d65bdefa38d06bc6b1d62398
Kevin Luna <kevin.luna200344@gmail.com>
2022-01-19 16:08:57 -0600

    If the minecraft version is incorrect, stop the script

--- MCW-Backup.ps1
+++ MCW-Backup.ps1
@@ -16,9 +16,14 @@ if($MinecraftVersion -eq "Bedrock"){
     $minecraft_worlds_folder = "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds"
 }elseif($MinecraftVersion -eq "Java"){
     $minecraft_worlds_folder = "C:\Users\Kevin\AppData\Roaming\.minecraft\saves"
+}else {
+    Write-Error "Minecraft version unrecognized, plase choose one of the follow: Bedrock, Java"
+    exit
 }
 
 Set-Alias -Name 7zip -Value "C:\Program Files\7-Zip\7z.exe"
+
+
 $date =  Get-Date -Format "dd-MM-yyyy"
 $output_filename = "$Output\Pichichi-$date.7z"
 
